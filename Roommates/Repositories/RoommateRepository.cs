9dd233327d6ae32efd2acab02db7ecc9eeb791a1
ActuallyAJL <zero3260@hotmail.com>
2022-06-09 15:57:29 -0500

    completed steps one and two

---
+++ Roommates/Repositories/RoommateRepository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Data.SqlClient;
+using Roommates.Models;
+using System.Collections.Generic;
+
+namespace Roommates.Repositories
+{
+    public class RoommateRepository : BaseRepository
+    {
+        public RoommateRepository(string connectionString) : base(connectionString) { }
+     
+        public Roommate GetById(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT Roommate.FirstName, Roommate.LastName, Roommate.RentPortion, Room.Name FROM Room JOIN Roommate ON Roommate.RoomId = Room.Id WHERE Roommate.Id = @id";
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        Roommate roommate = null;
+
+                        // If we only expect a single row back from the database, we don't need a while loop.
+                        if (reader.Read())
+                        {
+                            Room room = new Room
+                            {
+                                Name = reader.GetString(reader.GetOrdinal("Name"))
+                            };
+                            roommate = new Roommate
+                            {
+                                Id = id,
+                                FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                                LastName = reader.GetString(reader.GetOrdinal("LastName")),
+                                RentPortion = reader.GetInt32(reader.GetOrdinal("RentPortion")),
+                                Room = room
+                            };
+                        }
+                        return roommate;
+                    }
+
+                }
+            }
+        }
+
+        
+    }
+}
